c3f68c1279853beaced0df90fac1f1388b5cd9d3
Glen Leatherman <gleather1969@gmail.com>
2016-04-24 15:35:22 +0000

    adding project files

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on port ' + app.get('port') );
+});
+
