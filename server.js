1aa1ab775db4aee8ccd5ef2d929e3d9d949249c6
Glen Leatherman <gleather1969@gmail.com>
2016-04-24 16:48:49 +0000

    New server.js

--- server.js
+++ server.js
@@ -8,17 +8,23 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(function(req, res){
-	res.type('text/plain');
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
+});
+
+app.use(function(req, res, next){
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
